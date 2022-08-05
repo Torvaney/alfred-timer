1902a87dc2dc6bfb88269e4ba6de00d780137ee0
Ben Torvaney <Torvaney@users.noreply.github.com>
2022-08-05 16:22:29 +0100

    Use launchctl instead of launchd
    Using the right command can be helpful. Closes #3 (hopefully...)

--- notify.sh
+++ notify.sh
@@ -3,5 +3,5 @@ osascript -e \
     "tell application id \"com.runningwithcrayons.Alfred\" to run trigger \"timer-notification\" in workflow \"io.github.torvaney.alfred-timer\" with argument \"$1\"";
 
 # Cleanup script by unloading the job and deleting the plist file
-launchd bootout gui/$UID/$2;
+launchctl bootout gui/$UID/$2;
 rm $3;
