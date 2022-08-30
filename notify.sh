b06f54cd6bdb55200f496be3eb2cc4b91e2ae9c9
Ben Torvaney <torvaney@protonmail.com>
2022-08-30 14:18:34 +0100

    Remove plist before bootout

--- notify.sh
+++ notify.sh
@@ -2,6 +2,6 @@
 osascript -e \
     "tell application id \"com.runningwithcrayons.Alfred\" to run trigger \"timer-notification\" in workflow \"io.github.torvaney.alfred-timer\" with argument \"$1\"";
 
-# Cleanup script by unloading the job and deleting the plist file
-launchctl bootout gui/$UID/$2;
+# Cleanup script by deleting the plist file and unloading the job
 rm $3;
+launchctl bootout gui/$UID/$2;
